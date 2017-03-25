ed4c0a46be267cfce00d16be9d0bb28b8cf71051
周鲁东 <ludongzhou@gmail.com>
2017-03-25 16:53:32 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/tiger.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/zhouludong/ClionProjects/tiger/chap1/main.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap1/main.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap1/prog1.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap1/prog1.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap1/slp.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap1/slp.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap1/util.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap1/util.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap10/graph.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap10/graph.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap12/runtime.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap12/runtime.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap2/driver.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap2/driver.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap2/errormsg.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap2/errormsg.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap2/util.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap2/util.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap3/errormsg.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap3/errormsg.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap3/lex.yy.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap3/lex.yy.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap3/parsetest.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap3/parsetest.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap3/util.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap3/util.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap4/absyn.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap4/absyn.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap4/lex.yy.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap4/lex.yy.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap4/parse.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap4/parse.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap4/prabsyn.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap4/prabsyn.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap4/symbol.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap4/symbol.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap4/table.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap4/table.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap5/types.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap5/types.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap6/temp.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap6/temp.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap7/printtree.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap7/printtree.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap7/tree.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap7/tree.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap8/canon.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap8/canon.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap9/assem.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap9/assem.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap9/canon.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap9/canon.c.o"
+  "/Users/zhouludong/ClionProjects/tiger/chap9/main.c" "/Users/zhouludong/ClionProjects/tiger/cmake-build-debug/CMakeFiles/tiger.dir/chap9/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/tiger.dir/cmake_clean.cmake
@@ -0,0 +1,36 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tiger.dir/chap1/main.c.o"
+  "CMakeFiles/tiger.dir/chap1/prog1.c.o"
+  "CMakeFiles/tiger.dir/chap1/slp.c.o"
+  "CMakeFiles/tiger.dir/chap1/util.c.o"
+  "CMakeFiles/tiger.dir/chap10/graph.c.o"
+  "CMakeFiles/tiger.dir/chap12/runtime.c.o"
+  "CMakeFiles/tiger.dir/chap2/driver.c.o"
+  "CMakeFiles/tiger.dir/chap2/errormsg.c.o"
+  "CMakeFiles/tiger.dir/chap2/util.c.o"
+  "CMakeFiles/tiger.dir/chap3/errormsg.c.o"
+  "CMakeFiles/tiger.dir/chap3/lex.yy.c.o"
+  "CMakeFiles/tiger.dir/chap3/parsetest.c.o"
+  "CMakeFiles/tiger.dir/chap3/util.c.o"
+  "CMakeFiles/tiger.dir/chap4/absyn.c.o"
+  "CMakeFiles/tiger.dir/chap4/lex.yy.c.o"
+  "CMakeFiles/tiger.dir/chap4/parse.c.o"
+  "CMakeFiles/tiger.dir/chap4/prabsyn.c.o"
+  "CMakeFiles/tiger.dir/chap4/symbol.c.o"
+  "CMakeFiles/tiger.dir/chap4/table.c.o"
+  "CMakeFiles/tiger.dir/chap5/types.c.o"
+  "CMakeFiles/tiger.dir/chap6/temp.c.o"
+  "CMakeFiles/tiger.dir/chap7/printtree.c.o"
+  "CMakeFiles/tiger.dir/chap7/tree.c.o"
+  "CMakeFiles/tiger.dir/chap8/canon.c.o"
+  "CMakeFiles/tiger.dir/chap9/assem.c.o"
+  "CMakeFiles/tiger.dir/chap9/canon.c.o"
+  "CMakeFiles/tiger.dir/chap9/main.c.o"
+  "tiger.pdb"
+  "tiger"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/tiger.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
